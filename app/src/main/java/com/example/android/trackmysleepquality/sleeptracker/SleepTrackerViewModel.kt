bbc7b915fd18ddb1872fbfb80825193047781953
lewvi <lew_rip@hotmail.com>
2020-08-05 16:22:08 +0700

    6.3.5_Step 1: Update button states

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -121,6 +121,16 @@ class SleepTrackerViewModel(
 
     }
 
+    val startButtonVisible = Transformations.map(tonight) {
+        it == null
+    }
+    val stopButtonVisible = Transformations.map(tonight) {
+        it != null
+    }
+    val clearButtonVisible = Transformations.map(nights) {
+        it?.isNotEmpty()
+    }
+
 
 }
 
